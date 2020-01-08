7efa2897725ebe9b83c0f33152dbdcb54e359786
Alexis Priore <alexispriore@gmail.com>
2020-01-08 17:07:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,3 +5,10 @@ school.roster
 school.add_student("Zach Morris", 9)
 school.roster
 
+hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
+ 
+hash
+ => {"new_key"=>["new_value_for_value_array"]}
+ 
+ 
